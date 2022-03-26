0d4e2b88cc508f35e172d0f813729ad2459b2343
Victor <vvsh1978@mail.ru>
2022-03-26 12:49:35 +0500

    feat: make lesson02

---
+++ script.js
@@ -0,0 +1,16 @@
+const num = 266219;
+let stringNum = String(num);
+let multiply = 1;
+
+for (let i = 0; i < stringNum.length; i++) {
+    multiply *= Number(stringNum[i])
+}
+
+console.log(multiply);
+
+let exponent = multiply**3
+
+console.log(exponent)
+
+console.log(String(exponent).substring(0,2))
+

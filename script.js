651cc2541464c94e2767d70c0908c5e4dde2b3d4
svv <vvsh1978@mail.ru>
2022-03-28 22:42:28 +0500

    feat: make hard lesson03

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const num = 266219;
-let stringNum = String(num);
-let multiply = 1;
+'use strict';
 
-for (let i = 0; i < stringNum.length; i++) {
-    multiply *= Number(stringNum[i])
+// Задание 1
+
+let lang = prompt('Введите язык', 'ru');
+
+// if else
+
+if (lang === 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+} else if (lang === 'en') {
+    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday')
+} else {
+    console.log('Вы ввели неверное значение')
 }
 
-console.log(multiply);
+// switch case
 
-let exponent = multiply**3
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break;
+    case 'en':
+        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+        break;
+    default:
+        console.log('Вы ввели неверное значение');
+}
 
-console.log(String(exponent).substring(0,2))
+// array
 
+let myArray = [['ru', 'en'], ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье', 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday']];
+
+console.log(myArray[1][myArray[0].indexOf(lang)]);
+
+// Задание 2
+
+let namePerson = prompt('Введите имя', 'Артем');
+
+let result = namePerson === 'Артем' ? 'директор' : (namePerson === 'Александр') ? 'преподаватель' : 'студент';
+console.log(result)

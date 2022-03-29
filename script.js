412ea15bf467452c0bac8e904ffd2b122ed4a46c
svv <vvsh1978@mail.ru>
2022-03-29 21:56:46 +0500

    feat: make lesson04

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
 'use strict';
 
-// Задание 1
+const dataInput = '   Это длинная строка больше 30-ти символов    ';
 
-let lang = prompt('Введите язык', 'ru');
-
-// if else
-
-if (lang === 'ru') {
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
-} else if (lang === 'en') {
-    console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday')
-} else {
-    console.log('Вы ввели неверное значение')
+const processData = (data) => {
+    if (typeof data != 'string') return 'Это не строка!';
+    data = data.trim();
+    return (data.length < 30) ? data : data.substr(0, 30) + '...'
 }
 
-// switch case
-
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-        break;
-    case 'en':
-        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-        break;
-    default:
-        console.log('Вы ввели неверное значение');
-}
-
-// array
-
-let myArray = [['ru', 'en'], ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье', 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday']];
-
-console.log(myArray[1][myArray[0].indexOf(lang)]);
-
-// Задание 2
-
-let namePerson = prompt('Введите имя', 'Артем');
-
-let result = namePerson === 'Артем' ? 'директор' : (namePerson === 'Александр') ? 'преподаватель' : 'студент';
-console.log(result)
+console.log(processData(dataInput))

5ab8934bff89baa787c6146eb08b579a09c1f648
svv <vvsh1978@mail.ru>
2022-04-01 21:51:01 +0500

    feat: make lesson07

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 'use strict';
 
-// Фильтр чисел из массива
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const today = new Date().getDay();
 
-const arr = ['34534', '4643', '145345', '234534', '42345', '56456', '45645'];
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] === '2' || arr[i][0] === '4') {
-        console.log(arr[i]);
+week.forEach(function (item, index) {
+    if (index === today - 1 && (index === 5 || index === 6)) {
+        console.log('%c' + item, 'font-weight: bold; font-style: italic');
+    } else if (index === (today - 1)) {
+        console.log('%c' + item, 'font-weight: bold');
+    } else if (index === 5 || index === 6) {
+        console.log('%c' + item, 'font-style: italic');
+    } else {
+        console.log(item);
     }
-}
-
-
-// Простые числа
-
-const n = 100;
-let result = [2]
-
-for (let i = 3; i <= n; i++) {
-    let count = 0;
-    for (let j = 2; j < i; j++) {
-        if (i % j === 0) count++;
-    }
-    if (count === 0) result.push(i);
-}
-
-for (let i = 0; i < result.length; i++) {
-    console.log(result[i] + ' Делители этого числа: 1 и ' + result[i])
-}
+})
